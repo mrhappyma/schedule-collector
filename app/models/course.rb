10528ea9d98b527cc46c9358263c38dc47487289
Dominic Ruggiero <dominic@userexe.me>
2024-08-08 13:11:06 -0400

    allow - in course name

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,7 @@ class Course < ApplicationRecord
   has_many :schedule_courses, dependent: :destroy
   has_many :schedules, through: :schedule_courses
 
-  validates :name, presence: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z&()0-9\s]+\z/, message: "ocontains forbidden characters" }
+  validates :name, presence: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z&()0-9\-\s]+\z/, message: "ocontains forbidden characters" }
   validates :teacher, presence: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z,\s]+\z/, message: "only allows letters & spaces & commas" }
   validates :period, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 9 }, exclusion: { in: [3], message: "doesn't allow period 3" }
   validates :room, presence: true, numericality: { only_integer: true, greater_than: 100, less_than: 999 }
