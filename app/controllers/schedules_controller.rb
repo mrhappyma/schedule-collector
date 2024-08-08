894b379a8126d6937675bcb4410081b5caf194f0
Dominic Ruggiero <dominic@userexe.me>
2024-08-07 21:03:48 -0400

    schedule add form

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,2 +1,29 @@
 class SchedulesController < ApplicationController
+  def show
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def new
+    @schedule = Schedule.new
+    @courses = Course.all.sort_by(&:period)
+  end
+
+  def create
+    courses = Course.where(id: params[:schedule][:course_ids])
+    sparams = schedule_params
+    sparams[:courses] = courses
+    @schedule = Schedule.new(sparams)
+    if @schedule.save
+      redirect_to @schedule
+    else
+      @courses = Course.all.sort_by(&:period)
+      render :new
+    end
+  end
+
+  private
+
+  def schedule_params
+    params.require(:schedule).permit(:name, :grade)
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,8 @@
 class Schedule < ApplicationRecord
-  has_many :schedule_courses
+  has_many :schedule_courses, dependent: :destroy
   has_many :courses, through: :schedule_courses
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z,\s]+\z/, message: "only allows letters & spaces & commas" }
+  validates :grade, presence: true, numericality: { only_integer: true, greater_than: 8, less_than: 13, message: "must be between 9 and 12" }
+  validates :courses, presence: true, length: {is: 7, message: "must have 7 courses"}, on: :create
 end
