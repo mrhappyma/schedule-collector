612b699d3a8562a55950bd8293d14ff035afe931
Dominic Ruggiero <dominic@userexe.me>
2024-08-08 14:41:43 -0400

    room numbers less than 1000

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,7 +7,7 @@ class Course < ApplicationRecord
   validates :name, presence: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z&()0-9\-\s]+\z/, message: "ocontains forbidden characters" }
   validates :teacher, presence: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z,\s]+\z/, message: "only allows letters & spaces & commas" }
   validates :period, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 9 }, exclusion: { in: [3], message: "doesn't allow period 3" }
-  validates :room, presence: true, numericality: { only_integer: true, greater_than: 99, less_than: 999 }
+  validates :room, presence: true, numericality: { only_integer: true, greater_than: 99, less_than: 1000 }
 
   validates_uniqueness_of :name, scope: :period, message: "already exists in this period"
 
