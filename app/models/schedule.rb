0f18c0b9ebc6ca34323a613313bc66a554982cdb
Dominic Ruggiero <dominic@userexe.me>
2024-08-08 15:09:36 -0400

    allow - in name

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,7 +2,7 @@ class Schedule < ApplicationRecord
   has_many :schedule_courses, dependent: :destroy
   has_many :courses, through: :schedule_courses
 
-  validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z,\s]+\z/, message: "only allows letters & spaces & commas" }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z-,\s]+\z/, message: "only allows letters & spaces & commas" }
   validates :grade, presence: true, numericality: { only_integer: true, greater_than: 8, less_than: 13, message: "must be between 9 and 12" }
   validates :courses, presence: true, on: :create
 end
