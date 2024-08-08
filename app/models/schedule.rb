d1ce844ae93932b3ca3d7ce595f1b57df4358c39
Dominic Ruggiero <dominic@userexe.me>
2024-08-08 15:15:48 -0400

    tell people to yell at me if they have less than 7 courses

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,5 +4,5 @@ class Schedule < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 100}, format: { with: /\A[a-zA-Z-,\s]+\z/, message: "only allows letters & spaces & commas" }
   validates :grade, presence: true, numericality: { only_integer: true, greater_than: 8, less_than: 13, message: "must be between 9 and 12" }
-  validates :courses, presence: true, length: {is: 7, message: "must have 7 courses"}, on: :create
+  validates :courses, presence: true, length: {is: 7, message: "must have 7 courses - if you have less, ask dominic to add your schedule"}, on: :create
 end
